6a271aabb95d8369f5b1a4374b8dcb32c4c6b32e
Sandra <sandra.alpha2010@gmail.com>
2020-07-07 15:00:12 -0700

    connection.js complete, skeleton fo orm.js complete

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+//Set up mysql database connection
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "area",
+    database: "burgerDB"
+});
+
+//Make connection
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//Export connection to orm.js
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,15 @@
+//Object for all SQL query statements in the form of functions
+
+const orm = {
+    all: function() {
+
+    },
+    create: function() {
+
+    },
+    update: function() {
+
+    }
+};
+//Export orm object to be used in models folder, burger.js
+module.exports = orm;
