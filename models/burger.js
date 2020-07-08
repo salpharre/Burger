2fffe2c6cef8d0f7a41ec0903e0029447cb5b541
Sandra <sandra.alpha2010@gmail.com>
2020-07-07 23:42:51 -0700

    added temporatry html to make sure jquery works as aspected

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+//Import orm module to create functions that will interact with database
+const orm = require("../config/orm.js");
+
+const burger = {
+    
+}
+
+//Export burger object containing database functions to controller, burgerController.js
+module.exports = burger;
