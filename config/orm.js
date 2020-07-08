34096434a8f83071d68206ae2d1cee5f46317e2a
Sandra <sandra.alpha2010@gmail.com>
2020-07-07 23:35:17 -0700

    orm.js complete

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,81 @@
+//Importing MySQL connection
+const connection = require("../config/connection.js");
+
+//Helper function for SQL query statements' syntax
+function questionMarks(num) {
+    const arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+//Helper function to convert object key/value into SQL syntax
+function objToSql(ob) {
+    const arr = [];
+
+    // loop through the keys and push the key/value as a string into arr
+    for (var key in ob) {
+        const value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            //push key and value as a string to arr
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 //Object for all SQL query statements in the form of functions
-
 const orm = {
-    all: function() {
-
+    all: function (tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
-    create: function() {
+    create: function (table, cols, vals, cb) {
+        const queryString = "INSERT INTO " + table;
 
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            //this is executing the callback function from cat.js, and the function there is executing the cat.create function in catsController.js
+            cb(result);
+        });
     },
-    update: function() {
+    update: function (table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
 
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     }
 };
 //Export orm object to be used in models folder, burger.js
