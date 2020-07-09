72aeff18809a18240aeb97fb60a7a319215dc083
Sandra <sandra.alpha2010@gmail.com>
2020-07-08 17:44:44 -0700

    added temporary html file to test appearance of webpage, edited jqueryburger.js and burger css

---
+++ public/assets/js/jQueryBurger.js
@@ -0,0 +1,27 @@
+//jQuery version of document.ready, to make sure DOM is fully loaded before attaching handlers
+$(function() {
+//Click event to add a new burger to database and reload page once POST request is sent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
